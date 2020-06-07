205d3b184a9534f276ac3bea78c742dbdbe2726c
Marcel Cordeiro <marcelfcordeiro@gmail.com>
2020-06-07 15:16:12 -0300

    connect database, typeorm and migrations

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,34 +1,18 @@
 import Appointment from '../models/appointment'
-import { isEqual } from 'date-fns'
 
-interface CreateAppointmentDTO {
-    provider: string
-    date: Date
-}
+import { EntityRepository, Repository } from 'typeorm'
 
-class AppointmentsRepository {
-    private appointments: Appointment[]
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
 
-    constructor() {
-        this.appointments = []
+    public async findByDate(date: Date): Promise<Appointment | null> {
+        const findAppointment = await this.findOne({
+            where: { date }
+        })
+
+        return findAppointment || null
     }
 
-    public all(): Appointment[] {
-        return this.appointments
-    }
-
-    public findByDate(date: Date): Appointment | null {
-        const appointment = this.appointments.find(appointment => isEqual(date, appointment.date))
-        return appointment || null
-    }
-
-    public create({ provider, date }: CreateAppointmentDTO): Appointment {
-        const appointment = new Appointment({ provider, date })
-
-        this.appointments.push(appointment)
-
-        return appointment
-    }
 }
 
 export default AppointmentsRepository
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import express from 'express';
-import routes from './routes';
+import 'reflect-metadata'
 
-const app = express();
+import express from 'express'
+import routes from './routes'
 
-app.use(express.json());
-app.use(routes);
+import './database'
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333!');
-});
+    console.log('Server started on port 3333!')
+})
 
